4f1f696803dc3d257056ffb5ea0775ef2ff0a5ac
Nemo <thereisnotruth12@gmail.com>
2021-09-21 00:01:16 +0900

    Solve Baekjoon P2438

---
+++ Baekjoon/P2438/Main.java
@@ -0,0 +1,17 @@
+package P2438;
+
+import java.util.*;
+
+public class Main {
+    static int N;
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
