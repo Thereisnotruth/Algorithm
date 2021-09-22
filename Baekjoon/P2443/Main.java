48283221205e201d650bcb514486069d43c23c1f
Nemo <thereisnotruth12@gmail.com>
2021-09-23 04:30:39 +0900

    Solve Baekjoon P2443

---
+++ Baekjoon/P2443/Main.java
@@ -0,0 +1,24 @@
+package P2443;
+
+import java.util.*;
+
+public class Main {
+    static int N;
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+
+        for (int i = N; i > 0; i--) {
+            for (int j = N; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 2 * i - 1; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
