0d70d5c69eeeb59b100d1ea74ff52d9e30fe0fd8
Nemo <thereisnotruth12@gmail.com>
2021-09-24 23:33:28 +0900

    Solve Baekjoon P1475

---
+++ Baekjoon/P1475/Main.java
@@ -0,0 +1,41 @@
+package P1475;
+
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static String N;
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextLine();
+        arr = new int[9];
+        
+        int stack = 0;
+        for (int i = 0; i < N.length(); i++) {
+            if (N.charAt(i) == '6' || N.charAt(i) == '9') {
+                stack++;
+                if (stack == 2) {
+                    stack = 0;
+                    arr[6]++;
+                }
+            } else {
+                arr[Integer.parseInt(String.valueOf(N.charAt(i)))]++;
+            }
+        }
+
+        if (stack == 1) {
+            arr[6]++;
+        }
+
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+    
+}
