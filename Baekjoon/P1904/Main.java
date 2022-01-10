9010b2a61644c579fcc4ce1fc63144050cd9601d
Nemo <thereisnotruth12@gmail.com>
2022-01-11 00:07:59 +0900

    Solve Baekjoon P1904

---
+++ Baekjoon/P1904/Main.java
@@ -0,0 +1,22 @@
+package P1904;
+
+import java.util.*;
+
+public class Main {
+  static int n;
+  static int[] dp;
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    dp = new int[n + 1];
+
+    dp[1] = 1;
+    if (n > 1) dp[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+      dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+    }
+    System.out.println(dp[n]);
+  }
+  
+}
