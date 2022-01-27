81bb7f1bbcfd3ed4dbda59123c7eee676ca24794
Nemo <thereisnotruth12@gmail.com>
2022-01-28 03:19:46 +0900

    Solve Programmers N으로표현

---
+++ Programmers/DFS/N으로표현/Solution.java
@@ -0,0 +1,35 @@
+package DFS.N으로표현;
+
+import java.util.*;
+
+public class Solution {
+  static int min = Integer.MAX_VALUE;
+  public int solution(int N, int number) {
+    dfs(0, N, number, 0);
+    if (min == Integer.MAX_VALUE) {
+      return -1;
+    }
+    return min;
+  }
+  public void dfs(int depth, int N, int number, int cur) {
+    if (depth > 8) {
+      return;
+    }
+    if (number == cur) {
+      min = Math.min(depth, min);
+      return;
+    }
+
+    int tmp = 0;
+
+    for (int i = 0; i < 8; i++) {
+      if (depth + i < 8) {
+        tmp = tmp * 10 + N;
+        dfs(depth + i + 1, N, number, cur + tmp);
+        dfs(depth + i + 1, N, number, cur - tmp);
+        dfs(depth + i + 1, N, number, cur * tmp);
+        dfs(depth + i + 1, N, number, cur / tmp);
+      }
+    }
+  }
+}
---
+++ Programmers/Solve/Main.java
@@ -0,0 +1,12 @@
+package Solve;
+
+import DFS.N으로표현.Solution;
+
+public class Main {
+  public static void main (String[] args) {
+
+    Solution s = new Solution();
+    System.out.println(s.solution(5, 12));
+  }
+  
+}
