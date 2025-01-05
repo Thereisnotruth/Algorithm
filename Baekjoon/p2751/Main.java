8dc98664e3b7fa166bb30361587a56fd27b79aa3
Thereisnotruth <thereisnotruth12@gmail.com>
2025-01-06 01:14:15 +0900

    solve baekjoon 2751

---
+++ Baekjoon/p2751/Main.java
@@ -0,0 +1,33 @@
+package p2751;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        array = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(array);
+
+        for (int i = 0; i < N; i++) {
+            sb.append(array[i]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
