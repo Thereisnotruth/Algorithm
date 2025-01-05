236d5cd6cacf5952d942e6f069d46e0f1d886211
Thereisnotruth <thereisnotruth12@gmail.com>
2025-01-05 22:39:43 +0900

    solve baekjoon 1914

--- Baekjoon/P1914/Main.java
+++ Baekjoon/P1914/Main.java
@@ -7,38 +7,42 @@ import java.math.*;
 public class Main {
     static int N;
     static BigInteger cnt = new BigInteger("1");
-    static StringBuilder sb = new StringBuilder();
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        
-
-        N = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        N = Integer.parseInt(br.readLine());
 
         if (N == 1) {
-            System.out.println(1);
+            bw.write("1\n");
         } else {
             for (int i = 0; i < N; i++) {
                 cnt = cnt.multiply(new BigInteger("2"));
             }
             cnt = cnt.subtract(new BigInteger("1"));
-            System.out.println(cnt);
+            bw.write(cnt + "\n");
         }
         if (N <= 20) {
             hanoi(1, 2, 3, N);
         }
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
-    static void hanoi (int start, int mid, int end, int n) {
+    static void hanoi(int start, int mid, int end, int n) throws Exception {
+        if (n == 0)
+            return;
         if (n == 1) {
             if (N <= 20) {
-                System.out.println(start + " " + end);
+                bw.write(start + " " + end + "\n");
             }
         } else {
             hanoi(start, end, mid, n - 1);
             if (N <= 20) {
-                System.out.println(start + " " + end);
-            }            
+                bw.write(start + " " + end + "\n");
+            }
             hanoi(mid, start, end, n - 1);
         }
     }
